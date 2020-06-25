b9f471dec8761ca81abdd2440658e97aec6e371a
anonyymi <anonyymigithub@gmail.com>
2020-06-25 21:15:10 +0300

    use localstack to simulate the missing parts of the aws cloud in local env

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lautasofta-service
+
+## development
+
+Local environment is setup so that docker is used to simulate the aws cloud.
+
+Install+configure aws cli, aws sam cli, docker, localstack & flyway-cli to get started.
--- run_local.sh
+++ run_local.sh
@@ -2,10 +2,13 @@
 set -e
 
 # db
-docker-compose -f local_env.yaml up -d
+docker-compose -f local_env.yaml up -d --force-recreate localstack
 flyway -configFiles=./database/flyway.conf clean
 flyway -configFiles=./database/flyway.conf migrate
 
 # app
 sam build --use-container
+aws --endpoint-url http://localhost:4581 cloudformation deploy \
+    --template-file "./.aws-sam//build/template.yaml" \
+    --stack-name "lautasofta-service"
 sam local start-api --env-vars local_env.json --docker-network host
