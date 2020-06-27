465f6d21fed5fd42532a9dc6d7452844da885173
anonyymi <anonyymigithub@gmail.com>
2020-06-28 00:49:40 +0300

    specify --use-container when building sam app for local apigw env

--- run_local.sh
+++ run_local.sh
@@ -6,5 +6,5 @@ set -e
 aws --endpoint-url http://localhost:4566 s3api list-objects --bucket lautasofta-local-media --query "(Contents)[].[Key]" --output text | while read line ; do aws --endpoint-url http://localhost:4566 s3api put-object-acl --acl public-read --bucket lautasofta-local-media --key $line ; done
 
 # app
-sam build
+sam build --use-container
 sam local start-api --env-vars local_env.json --port 3002 --docker-network host
