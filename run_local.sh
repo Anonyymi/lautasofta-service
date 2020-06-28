975b9910a196b82959af9a66802e3a4c879ce55b
anonyymi <anonyymigithub@gmail.com>
2020-06-28 16:45:11 +0300

    reorganize scripts

--- run_local.sh
+++ run_local.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
-set -e
 
 # temp fix: update object acls
 # issue: issue: https://github.com/localstack/localstack/issues/2407
 aws --endpoint-url http://localhost:4566 s3api list-objects --bucket lautasofta-local-media --query "(Contents)[].[Key]" --output text | while read line ; do aws --endpoint-url http://localhost:4566 s3api put-object-acl --acl public-read --bucket lautasofta-local-media --key $line ; done
 
 # app
-sam build --use-container
-sam local start-api --env-vars local_env.json --port 3002 --docker-network host
+FLASK_APP=./service/api/app.py flask run --port 3002
